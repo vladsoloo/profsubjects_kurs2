dcc182a8fa74b03e61d6557e4c567cccc0949d26
vladsoloo <zhfedor33@gmail.com>
2025-10-30 14:28:31 +0300

    сделал задачу 3

--- cpp_30.10/zadacha3.cpp
+++ cpp_30.10/zadacha3.cpp
@@ -7,10 +7,10 @@ int main() {
     double n;
     cin >> n;
     
-    // Округляем до двух знаков после запятой
+    
     double rounded = round(n * 100) / 100;
     
-    // Выводим с фиксированной точностью и 2 знаками после запятой
+  
     cout << fixed << setprecision(2);
     cout << rounded << endl;
     
